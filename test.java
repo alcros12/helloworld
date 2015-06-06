b8268120c18bd510251c2d032e80c8314123bb29
Carlos <alcros_12@hotmail.com>
2015-06-05 23:03:33 -0500

    agrega *

--- test.java
+++ test.java
@@ -1,8 +1,8 @@
 public class test {
 
  	public static void main(String args[]){
-  	  System.out.println("**************************");
-  	  System.out.println("*Hola mundo, Hola jenkins*");
-  	  System.out.println("**************************");
+  	  System.out.println("****************************");
+  	  System.out.println("**Hola mundo, Hola jenkins**");
+  	  System.out.println("****************************");
 	 }
 }
